0f750447e5f768b7ef60aed7d424dfbbccc1fe46
LouisFores <lephamminhhieu2903@gmail.com>
2023-11-21 09:23:32 +0700

    123

--- src/main/java/com/example/webwhatforlunch/service/BillDAO.java
+++ src/main/java/com/example/webwhatforlunch/service/BillDAO.java
@@ -12,7 +12,7 @@ import java.util.List;
 public class BillDAO {
     private String username = "root";
 
-    private String password = "1111";
+    private String password = "mySQL7122023@";
 
     private String jdbcURL = "jdbc:mysql://localhost:3306/WebWhatForLunch";
 
